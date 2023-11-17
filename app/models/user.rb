acdb3f85dae263bc2006e388ca655df342ee121a
unknown <kudzpaul5@gmail.com>
2023-11-17 10:30:36 +0200

    linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,22 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-    validates :name, presence: true
-    validates :photo, presence: true
-    validates :bio, presence: true
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :bio, presence: true
 
-    before_create :set_created_at
-    before_save :set_updated_at
+  before_create :set_created_at
+  before_save :set_updated_at
 
-    private
+  private
 
-    def set_created_at
-        self.created_at ||= Time.current
-    end
+  def set_created_at
+    self.created_at ||= Time.current
+  end
 
-    def set_updated_at
-        self.updated_at = Time.current
-    end
+  def set_updated_at
+    self.updated_at = Time.current
+  end
 end
