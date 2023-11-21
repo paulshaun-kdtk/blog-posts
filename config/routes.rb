5452bd6596f772efd95e31b23159ec7a9eb8f4e2
unknown <kudzpaul5@gmail.com>
2023-11-21 08:30:06 +0200

    linters fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create destroy] do
-
-    end
+    resources :posts, only: %i[index show new create destroy]
   end
 
   get 'up' => 'rails/health#show', as: :rails_health_check
